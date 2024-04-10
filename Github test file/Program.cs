4bb11ecde6f0796a1c12e8fd1cf372c69bf7305e
Krisgeorge05 <165035149+Krisgeorge05@users.noreply.github.com>
2024-04-10 13:43:10 +0200

    Update Program.cs
    Modification to HelloWorld output.

--- Github test file/Program.cs
+++ Github test file/Program.cs
@@ -10,7 +10,7 @@ namespace Github_test_file
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! One more modification!");
         }
     }
 }
